25336ab6d3e4c333d7214f4d7aacc5268e0a59ea
MrP1xel <mr@p1xel.org>
2017-11-16 13:53:11 +0100

    Add files via upload
    * Some comments added
    * Remove some 'test' lines
    * handle connection error to API

--- rubrik-cnx.py
+++ rubrik-cnx.py
@@ -5,6 +5,7 @@ from pprint import pprint
 from requests.auth import HTTPBasicAuth
 requests.packages.urllib3.disable_warnings()
 
+
 class Rubrik():
         def __init__(self,ip,username,password):
                 self.ip = ip
@@ -13,24 +14,29 @@ class Rubrik():
                 self.password = password
                 self.api_url = self.url + "/api/v1"
                 self.login_url_end = "/login"
-                self.login_session_end = "/session"
                 self.login_url = self.api_url + self.login_url_end
 
-
-
         def auth(self):
-                login_header={'Content-Type':'application/json','Accept':'application/json'}
-                #userdata = "'{\"username\":\"%s\",\"password\":\"%s\"}'" % (self.username,self.password)
-                userdata = "{" + "\"username\":\""+self.username+"\",\"password\":\""+self.password+"\"}"
-                #print str(userdata)
-                #print self.login_url
-                #r = requests.post(self.login_url,headers=login_header,data='{"username":"admin","password":"Rotiontan"}',verify=False)
-                r = requests.post(self.login_url,headers=login_header,data=userdata,verify=False)
-                self.userID = (r.json().get('userId'))
-                self.token = (r.json().get('token'))
-                self.token_b64 = base64.b64encode(bytes(self.token))
-                self.auth_header = {"Authorization": "Basic " + self.token_b64.decode("utf-8")}
+                # Send username + password to /login API endpoint
+                # get the token
+                # create an header with "Authorization":"Basic" + token encoded with base64
 
-				
-rubrik_test = Rubrik("ip","user","password")
-rubrik_test.auth()
+                login_header={'Accept':'application/json'}
+                userdata = "{" + "\"username\":\""+self.username+"\",\"password\":\""+self.password+"\"}"
+                r = requests.post(self.login_url,headers=login_header,data=userdata,verify=False)
+                if r.status_code == 200:
+                        print "Connection OK"
+                        self.userID = (r.json().get('userId'))
+                        self.token = (r.json().get('token'))
+                        self.token_b64 = base64.b64encode(bytes(self.token))
+                        self.auth_header = {"Authorization": "Basic " + self.token_b64.decode("utf-8")}
+
+                else:
+                        if r.status_code == 400:
+                                print "The connection request is malformated"
+
+# To connect to your rubrik, just create a Rubrik object with following params : IP, username, password
+# Then call the auth() method of this object
+
+r_test = Rubrik("172.17.42.90","admin","Rotiontan")
+r_test.auth()
